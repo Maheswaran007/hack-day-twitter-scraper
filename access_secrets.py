7324230a17592ccad3feba24d41c636e93e37d0c
Maheswaran007 <maheswaran.jayagopan2602@gmail.com>
2022-11-24 16:09:56 +0100

    logged the env variales

--- access_secrets.py
+++ access_secrets.py
@@ -7,6 +7,7 @@ def access_secret():
     client = secretmanager.SecretManagerServiceClient()
     secret_name = os.getenv("SECRET_NAME", "")
     project_id =  os.getenv("PROJECT_ID", "")
+    print(secret_name, project_id)
     request = {"name": f"projects/{project_id}/secrets/{secret_name}/versions/latest"}
     response = client.access_secret_version(request)
     secret_string = response.payload.data.decode("UTF-8")
