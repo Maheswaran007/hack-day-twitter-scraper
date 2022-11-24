6d825ee47a912cfba93369f1f897bae91068821f
Maheswaran007 <maheswaran.jayagopan2602@gmail.com>
2022-11-24 15:55:10 +0100

    refactored the code and added env variables

---
+++ data_integration.py
@@ -0,0 +1,19 @@
+import os
+from google.cloud import bigquery
+import pandas
+from google.oauth2 import service_account
+from dotenv import load_dotenv
+load_dotenv()
+
+# CREDENTIALS GCP
+
+bigqueryClient = bigquery.Client()
+
+
+#UPDATE TABLE
+def update_table(df4):
+    
+    tableRef = bigqueryClient.dataset("Ikea_tweets").table("Ikea_hackdays_tb")
+    bigqueryJob = bigqueryClient.load_table_from_dataframe(df4, tableRef)
+    print('Successfully Added into Bigquery')
+    bigqueryJob.result()
--- main.py
+++ main.py
@@ -1,22 +1,57 @@
-import sys
-from flask import escape
-import functions_framework
+# import tweepy
+import tweepy as tw
+import pandas as pd
 import os
-# hello cruel world.
-@functions_framework.http
+from data_integration import update_table
+from dotenv import load_dotenv
+from access_secrets import access_secret
+load_dotenv()
+# your Twitter API key and API secret
+
+
+secrets = access_secret()
+
+my_api_key = secrets.get('API_KEY', '')
+my_api_secret = secrets.getenv('API_KEY_SECRET', '')
+# authenticate
+auth = tw.OAuthHandler(my_api_key, my_api_secret)
+api = tw.API(auth, wait_on_rate_limit=True)
+
+search_query = "ikea -filter:retweets"
+# get tweets from the API
+
 def twitter_scrapper(request):
-    """HTTP Cloud Function.
-    Args:
-        request (flask.Request): The request object.
-        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
-    Returns:
-        The response text, or any set of values that can be turned into a
-        Response object using `make_response`
-        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
-    Note:
-        For more information on how Flask integrates with Cloud
-        Functions, see the `Writing HTTP functions` page.
-        <https://cloud.google.com/functions/docs/writing/http#http_frameworks>
-    """
-    Test_API_key = os.getenv("Test_API_key", "")
-    return f'Hello World! {Test_API_key}'
+    tweets = tw.Cursor(api.search_tweets,
+                q=search_query,
+                lang="en").items(100)
+
+    # store the API responses in a list
+    tweets_copy = []
+    for tweet in tweets:
+        tweets_copy.append(tweet)
+        
+    print("Total Tweets fetched:", len(tweets_copy))
+    tweets_df = pd.DataFrame()
+    # populate the dataframe
+    for tweet in tweets_copy:
+        hashtags = []
+        try:
+            for hashtag in tweet.entities["hashtags"]:
+                hashtags.append(hashtag["text"])
+            text = api.get_status(id=tweet.id, tweet_mode='extended').full_text
+        except:
+            pass
+        tweets_df = pd.concat([tweets_df,pd.DataFrame.from_records([{'user_name': tweet.user.name, 
+                                                'user_location': tweet.user.location,\
+                                                'user_description': tweet.user.description,
+                                                'user_verified': tweet.user.verified,
+                                                'date': tweet.created_at,
+                                                'text': text, 
+                                                'hashtags': ' '.join(str(e) for e in hashtags),
+                                                'source': tweet.source}])])
+        tweets_df = tweets_df.reset_index(drop=True)
+
+
+
+    update_table(tweets_df)
+    return
