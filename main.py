7e6598f838b278434c485c49745e949969096eb7
Maheswaran007 <maheswaran.jayagopan2602@gmail.com>
2022-11-26 10:01:05 +0100

    fixed typo with date field and added success message

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def twitter_scrapper(request):
                                                 'user_location': tweet.user.location,\
                                                 'user_description': tweet.user.description,
                                                 'user_verified': tweet.user.verified,
-                                                'created_ssdate': tweet.created_at,
+                                                'created_date': tweet.created_at,
                                                 'text': text, 
                                                 'hashtags': ' '.join(str(e) for e in hashtags),
                                                 'source': tweet.source,
